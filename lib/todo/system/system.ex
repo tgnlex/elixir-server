1b46a7f992dd787b4fa6ae740cb7d52691ae0669
tgnlex <alexanderwebber76@gmail.com>
2024-05-07 21:28:40 -0700

    debugging + bug fixes

--- lib/todo/system/system.ex
+++ lib/todo/system/system.ex
@@ -2,6 +2,7 @@ defmodule Todo.System do
   def start_link do
     Supervisor.start_link(
       [
+        Todo.Task.Metrics,
         Todo.ProcessRegistry,
         Todo.Database,
         Todo.Cache
@@ -10,6 +11,6 @@ defmodule Todo.System do
     )
   end
   def init(_) do
-    Supervisor.init([Todo.Cache, Todo.Database, Todo.ProcessRegistry], strategy: :one_for_one)
+    Supervisor.init([Todo.Cache, Todo.Database, Todo.ProcessRegistry, Todo.Task.Metrics], strategy: :one_for_one)
   end
 end

ac5a2bd4a96a9ad62224ec883dc5e2cb36c68772
Alex Webber <alexanderwebber76@gmail.com>
2024-05-09 21:09:48 -0700

    refactored server
    changed todo.server from a genserver based implementation to an agent based
    implementations

--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -1,37 +1,27 @@
 defmodule Todo.Server do
-  use GenServer, restart: :temporary
+  use Agent, restart: :temporary
   def start_link(name) do
-    IO.puts("Starting to-do server for #{name}")
-    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
+    Agent.start_link(
+      fn ->
+        IO.puts("Starting to-do server for ${name}")
+        {name, Todo.Database.get(name) || Todo.List.new()}
+      end, 
+      name: via_tuple(name)
+    )
   end
+  def add_entry(todo_server, new_entries) do
+    Agent.cast(todo_server, fn {name, todo_list} -> 
+      new_list = Todo.List.add_entry(todo_list, new_entry)
+        Todo.Database.store(name, new_list)
+        {name, new_list}
+    end)
+  end
+  def entries(todo_server, date) do 
+    Agent.get(
+      todo_server, 
+      fn {_name, todo_list} -> Todo.List.entries(todo_list, date) end
+    )
+  end 
   defp via_tuple(name) do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
   end
-  def init(name) do
-    send(self(), :real_init)
-    register(self(), name)
-    {:ok, nil}
-  end
-  def handle_cast({:store, key, data}, state) do
-    spawn(fn ->
-      key
-      |> file_name()
-      |> File.write!(:erlang.term_to_binary(data))
-    end)
-    {:noreply, state}
-  end
-  def handle_call({:get, key}, caller, state) do
-    spawn(fn ->
-      data = case File.read(file_name(key)) do
-        {:ok, contents} -> :erlang.binary_to_term(contents)
-        _ -> nil
-      end
-      GenServer.reply(caller, data)
-    end)
-    {:noreply, state}
-
-  end
-  def handle_info(:real_init, name) do
-    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
-    end
-end
